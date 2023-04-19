392a9ca4d845b9ac82e9aaa888c527d50c962608
Smriti-Mhrz <smriti.maharjan@swifttech.com>
2023-04-19 02:45:22 -0700

    Working on dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navList=['Dashboard', 'Users', 'Products', 'Settings'];
+  return(
+  <div className="dashboard">
+    <div className="dashboard-list">
+        <ul>
+          <NavLink to='/'>
+            {navList.map((item)=>{
+              return(
+                {item}
+              )
+            })}
+          </NavLink>
+        </ul>
+    </div>
+  </div>  
+  );
 };
