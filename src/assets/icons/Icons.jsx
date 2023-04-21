1b22dc4380531a1a664eea62025a02f780fa5f09
Smriti-Mhrz <smriti.maharjan@swifttech.com>
2023-04-21 02:10:51 -0700

    dashboard search added

--- src/assets/icons/Icons.jsx
+++ src/assets/icons/Icons.jsx
@@ -1,18 +1,25 @@
-import { BiSearch, BiUserPlus, BiEdit } from "react-icons/bi";
-import { RiAdminFill } from "react-icons/ri";
+import { BiSearch, BiUserPlus, BiEdit,BiHomeAlt,BiDownArrow} from "react-icons/bi";
+import { RiAdminFill, RiProductHuntFill} from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { BsThreeDotsVertical, BsBell } from "react-icons/bs";
 import {
   AiOutlineDelete,
+  AiOutlineUser,
   AiOutlineUserDelete,
   AiOutlineEye,
   AiOutlineEyeInvisible,
+  AiOutlineSetting,
 } from "react-icons/ai";
 
 export {
+  AiOutlineUser,
   BiSearch,
+  BiDownArrow,
+  AiOutlineSetting,
+  RiProductHuntFill,
   BiUserPlus,
   BiEdit,
+  BiHomeAlt,
   RiAdminFill,
   RxHamburgerMenu,
   BsBell,
